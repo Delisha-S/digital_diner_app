649a5342cd7c6c545c01ba71a69362a56ce6f822
Delisha Shyam <delishashyam06@gmail.com>
2025-11-13 11:18:08 -0600

    chore: initial commit resolved .gitignore conflict

---
+++ lib/models/ai_response.dart
@@ -0,0 +1,21 @@
+﻿class AIResponse {
+  final String id;
+  final String text;
+  final Map<String, dynamic>? meta;
+
+  AIResponse({required this.id, required this.text, this.meta});
+
+  factory AIResponse.fromJson(Map<String, dynamic> json) {
+    return AIResponse(
+      id: json['id']?.toString() ?? '',
+      text: json['text']?.toString() ?? '',
+      meta: json['meta'] is Map<String, dynamic> ? Map<String, dynamic>.from(json['meta']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'text': text,
+        if (meta != null) 'meta': meta,
+      };
+}
